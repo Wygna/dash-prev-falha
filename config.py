a4527b53589df078a4d5b2f829870f126c66efd2
Wygna <47439743+Wygna@users.noreply.github.com>
2025-12-07 20:53:03 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,36 +1,22 @@
+import sqlite3
 import streamlit as st
-from sqlalchemy import text
+
 
 class CadastroDB:
     def __init__(self):
-        # Conecta ao PostgreSQL usando o secrets.toml
-        self.conn = st.connection("mydb", type="sql")
 
-    def criar_tabela(self):
-        query = text("""
-            CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                name TEXT,
-                password TEXT
-            )
+        self.mydb = st.connection("mydb", type="sql")
+        self.cursor = self.mydb.cursor()
+
+        # Criar tabela se não existir
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            password TEXT
+        )
         """)
-        with self.conn.session as s:
-            s.execute(query)
-            s.commit()
+        self.mydb.commit()
 
-    def inserir(self, name, password):
-        query = text("""
-            INSERT INTO users (name, password)
-            VALUES (:name, :password)
-        """)
-        with self.conn.session as s:
-            s.execute(query, {"name": name, "password": password})
-            s.commit()
-
-    def listar(self):
-        return self.conn.query("SELECT * FROM users;")
-        
-
-# Instância global
+# Instância global para reaproveitar
 db = CadastroDB()
-db.criar_tabela()
