214c173c458c6735a136aa987fece90edd922289
Wygna <47439743+Wygna@users.noreply.github.com>
2025-12-07 22:37:54 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,27 @@
-import os
 import streamlit as st
+import os
 
 class CadastroDB:
     def __init__(self):
 
-        # Conexão com PostgreSQL usando Streamlit
-        self.mydb = st.connection(
-            "mydb",
-            type="sql",
-            url=(
-                f"postgresql://{os.environ['PGUSER']}:{os.environ['PGPASSWORD']}"
-                f"@{os.environ['PGHOST']}:{os.environ['PGPORT']}/{os.environ['PGDATABASE']}"
-            )
-        )
+        self.mydb = st.connection(dialect = "postfresql"
+  host = os.environ['PGHOST'],
+  port = os.environ['PGPORT'],
+  database = os.environ['PGDATABASE'],
+  username = os.environ['PGUSER'], 
+  password = os.environ['PGPASSWORD'])
 
-        # Obtém um cursor (SQLAlchemy)
         self.cursor = self.mydb.cursor()
 
-        # Criar tabela se não existir
         self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                name TEXT,
-                password TEXT
-            )
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            password TEXT
+        )
         """)
 
         self.mydb.commit()
 
-
-# Instância global
-db = CadastroDB()
+   # Instância global para reaproveitar
+   db = CadastroDB()
