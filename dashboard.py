d7e7cc3c895fbe8a966dd84d4d8943bec2678594
Wygna <47439743+Wygna@users.noreply.github.com>
2025-12-08 00:55:52 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,8 @@ from config import db
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import os
+CadastroDB()
+cursor = CadastroDB.mydb.cursor()
 
 
 
